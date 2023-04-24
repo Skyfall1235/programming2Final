6910b68d766f888140ddb5a790e587ad42ba7989
Skyfall1235 <113722636+Skyfall1235@users.noreply.github.com>
2023-04-24 01:39:25 -0400

    setup of Scenes, and planning

--- root/finalProject/Assets/scripts/TowerMechanics/BasicTower.cs
+++ root/finalProject/Assets/scripts/TowerMechanics/BasicTower.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dartMonkey : MonoBehaviour
+public class BasicTower : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
